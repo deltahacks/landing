6f8340647e0d74a0bffa2cfce6ddc86c82a8ca7b
Kumail Naqvi <kumail@khanacademy.org>
2019-08-02 17:22:45 -0700

    init

---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <div>Home</div>
+    <div>About</div>
+    <div>Sponsors</div>
+    <div>FAQ</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Navigation',
+  components: {}
+})
+</script>
---
+++ src/components/Sponsors.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="flex-container">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Sponsors',
+  components: {}
+})
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: DodgerBlue;
+}
+
+.flex-container > div {
+  background-color: #f1f1f1;
+  width: 100%;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}
+</style>
+
