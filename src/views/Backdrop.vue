f0d7c1e756473a101b9607a833c5d04523c8ca9b
Anant Jain <anant.j2409@gmail.com>
2019-08-08 00:06:44 -0400

    Update Backdrop.vue

--- src/views/Backdrop.vue
+++ src/views/Backdrop.vue
@@ -1,26 +1,18 @@
 <template>
-<div id="app">
-	<div class="wrapper" id="d1">
+<div class="app">
+	<div class="section1" id="d1">
       <h1>{{scr}}</h1>
             </div>
             </div>
 </template>
 <style scoped>
-#d1{
+.section1{
 background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
+  /* background-color: rgb(69, 111, 225); */
+  height:5000px;
 }
-#d2,#d3,#d4,#d5{
-background:black;
-}
-.wrapper {
-  background-color: blue;
-  position: relative;
-  display: block;
-   background-blend-mode:overlay;
-     padding-top: 500vw;
-}
-.wrapper h1 {
-  position: absolute;
+h1 {
+  position: fixed;
   top: 10%;
   width: 70%;
   left: 0px;
@@ -51,17 +43,32 @@ export default Vue.extend({
 	},
   methods: {
   handleScroll () {
-    this.scrolled = window.scrollY > 0;
     this.scr=window.scrollY;
-    var a=1;
-    document.getElementById("d1").style.background="linear-gradient(to bottom, #252440"+(this.scr/10)+"%,#20525F"+(this.scr/20)+"%,#205273"+(this.scr/30)+"%,#205287"+(this.scr/40)+"%,#20529B"+(this.scr/50)+"%, #2052AF"+(this.scr/60)+"%, #2052C3"+(this.scr/70)+"%, #2052CD"+(this.scr/80)+"%, #2052E1"+(this.scr/90)+"%, #2052F5"+(this.scr/100)+"%)";
-    // document.getElementById("d2").style.opacity =590-(this.scr/(590)) 
+  var y = (window.scrollY || window.pageYOffset)
+   if(y<440){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%)";}
+  if(y>440){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,95,1) 0%, rgba(32,82,115,1)"+(y/20)+ "%)";}
+      if(y>880){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,115,1) 0%, rgba(32,82,135,1)"+(y/20)+ "%)";}
+      if(y>1320){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,135,1) 0%, rgba(32,82,155,1)"+(y/20)+ "%)";}
+    if(y>1760){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,155,1) 0%, rgba(32,82,175,1)"+(y/20)+ "%)";}
+  if(y>2200){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,175,1) 0%, rgba(32,82,195,1)"+(y/35)+ "%)";}
+  if(y>2640){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,195,1) 0%, rgba(32,82,205,1)"+(y/44)+ "%)";}
+       if(y>3080){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,205,1) 0%, rgba(32,82,225,1)"+(y/44)+ "%)";}
+       if(y>3520){
+     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,225,1) 0%, rgba(32,82,245,1)"+(y/44)+ "%)";}
 }},
 created () {
   window.addEventListener('scroll', this.handleScroll);
 },
 destroyed () {
-  // window.removeEventListener('scroll', this.handleScroll);
+  window.removeEventListener('scroll', this.handleScroll);
 },
 });
 
