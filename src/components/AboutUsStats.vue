8bb5b2a3966f20baf61d47c8b1a0dcc64b8f3328
gucci-ninja <beautyspell5@gmail.com>
2019-08-08 11:44:32 -0400

    using svg for circles

--- src/components/AboutUsStats.vue
+++ src/components/AboutUsStats.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="aligner">
-    <div class="aligner-item vector-container">
-      <svg width="400" height="400" transform="translate(0,60)">
-        <circle cx="200" cy="200" r="200" fill="#5799a9"  />
+  <div class="container">
+    <div class="container-item bubbles">
+      <svg width="360" height="360" transform="translate(0,60)">
+        <circle cx="180" cy="180" r="180" fill="#5799a9"  />
         <text x="50%" y="40%" text-anchor="middle" fill="white" font-size="140px" font-family="Arial" dy=".3em">700
         </text>
         <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="70px" font-family="Arial" dy=".3em">Hackers
         </text>
       </svg>
-      <svg width="300" height="300" transform="translate(-300,-380)" >
-        <circle cx="150" cy="150" r="100" fill="#5799a9"  />
-        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="100px" font-family="Arial" dy=".3em">36</text>
-        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="35px" font-family="Arial" dy=".3em">Hours</text>
+      <svg width="200" height="200" transform="translate(-130,-310)" >
+        <circle cx="100" cy="100" r="100" fill="#5799a9"  />
+        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="80px" font-family="Arial" dy=".3em">36</text>
+        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="30px" font-family="Arial" dy=".3em">Hours</text>
+      </svg>
+      <svg width="100" height="100" transform="translate(-240,-200)" >
+        <circle cx="50" cy="50" r="50" fill="#5799a9"  />
+        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="25px" font-family="Arial" dy=".3em">36</text>
+        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="10px" font-family="Arial" dy=".3em">Hours</text>
+      </svg>
+      <svg width="200" height="200" transform="translate(-550,-120)" >
+        <circle cx="100" cy="100" r="100" fill="#5799a9"  />
+        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="80px" font-family="Arial" dy=".3em">36</text>
+        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="30px" font-family="Arial" dy=".3em">Hours</text>
+      </svg>
+      <svg width="200" height="200" transform="translate(400,-520)" >
+        <circle cx="100" cy="100" r="100" fill="#5799a9"  />
+        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="80px" font-family="Arial" dy=".3em">36</text>
+        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="30px" font-family="Arial" dy=".3em">Hours</text>
+      </svg>
+      <svg width="200" height="200" transform="translate(180,-300)" >
+        <circle cx="100" cy="100" r="100" fill="#5799a9"  />
+        <text x="50%" y="45%" text-anchor="middle" fill="white" font-size="80px" font-family="Arial" dy=".3em">36</text>
+        <text x="50%" y="65%" text-anchor="middle" fill="white" font-size="30px" font-family="Arial" dy=".3em">Hours</text>
       </svg>
     </div>
   </div>
@@ -28,14 +48,14 @@ export default Vue.extend({
 
 <style>
 
-.aligner {
+.container {
   background-color: #356672;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.vector-container {
+.bubbles {
   width: 500px;
   height: 700px;
   position: relative;
