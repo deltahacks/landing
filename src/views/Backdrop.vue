4ee568a6a9882019118dfacbc551455fbb9d7e36
Anant Jain <anant.j2409@gmail.com>
2019-08-07 22:50:04 -0400

    Revert "Another Approach"
    This reverts commit 4936b11f785d0134bc7fc18a6d290d06794f63c2.

--- src/views/Backdrop.vue
+++ src/views/Backdrop.vue
@@ -1,48 +1,33 @@
 <template>
-<div>
-<section id="container">
-	<div>
-    <div class="full"><h1>Div1</h1></div>
-    <div class="full"><h1>Div2</h1></div>
-    <div class="full"><h1>Div3</h1></div>
-    <div class="full"><h1>Div4</h1></div>
-    <div class="full"><h1>Div5</h1></div>
-	</div>
-</section>
-</div>
+<div id="app">
+	<div class="wrapper" id="d1">
+      <h1>{{scr}}</h1>
+            </div>
+            </div>
 </template>
 <style scoped>
-h1{
-padding-top: 500px;
+#d1{
+background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
 }
-.full{
-  height: 500px;
+#d2,#d3,#d4,#d5{
+background:black;
+}
+.wrapper {
+  background-color: blue;
+  position: relative;
+  display: block;
+   background-blend-mode:overlay;
+     padding-top: 500vw;
+}
+.wrapper h1 {
+  position: absolute;
+  top: 10%;
+  width: 70%;
+  left: 0px;
+  margin-left: 15%;
   text-align: center;
-    vertical-align: middle
-}
-section {
-  /* background: linear-gradient(to bottom, #B721FF, #2AF598, #fec051, #FEE140, #FA709A); */
-    background: linear-gradient(to bottom, rgba(37,36,64,1), rgba(32,82,95,1), rgba(32,82,135,1) , rgba(32,82,175,1) , rgba(32,82,195,1));
-  background-size: 100%;
-  background-repeat: no-repeat;
-}
-
-
-#container {
-  background-color: #FFF;
-  /* margin: 80px 10%; */
-  /* padding: 30px 60px; */
-  border-radius: 20px;
-  box-shadow: 0 3px 26px 0 rgba(0, 0, 0, 0.2);
-  height: 500vh;
-}
-
-main {
-  background-color: #f7f7f7;
-  /* padding: 20px; */
-  border-radius: 10px;
-  color: #6b6b6b;
-  /* margin: 40px 0; */
+  display: block;
+  font-family: Josefin Sans;
 }
 
 </style>
@@ -50,34 +35,34 @@ main {
 import Vue from 'vue';
 
 
-export default Vue.extend({});
-//   name: "app",
-//   data() {
-// 		return{
-//          scrolled: false,
-//       description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens!',
-// 		bgc:{
-// 			backgroundColor: ''
-//     },
-//     fin: "background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);",
-//     scr: 0,
-//     col:'',
-//     }
-// 	},
-//   methods: {
-//   handleScroll () {
-//     this.scrolled = window.scrollY > 0;
-//     this.scr=window.scrollY;
-//     var a=1;
-//     document.getElementById("d1").style.background="linear-gradient(to bottom, #252440"+(this.scr/10)+"%,#20525F"+(this.scr/20)+"%,#205273"+(this.scr/30)+"%,#205287"+(this.scr/40)+"%,#20529B"+(this.scr/50)+"%, #2052AF"+(this.scr/60)+"%, #2052C3"+(this.scr/70)+"%, #2052CD"+(this.scr/80)+"%, #2052E1"+(this.scr/90)+"%, #2052F5"+(this.scr/100)+"%)";
-//     // document.getElementById("d2").style.opacity =590-(this.scr/(590)) 
-// }},
-// created () {
-//   window.addEventListener('scroll', this.handleScroll);
-// },
-// destroyed () {
-//   // window.removeEventListener('scroll', this.handleScroll);
-// },
-// });
+export default Vue.extend({
+  name: "app",
+  data() {
+		return{
+         scrolled: false,
+      description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens!',
+		bgc:{
+			backgroundColor: ''
+    },
+    fin: "background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);",
+    scr: 0,
+    col:'',
+    }
+	},
+  methods: {
+  handleScroll () {
+    this.scrolled = window.scrollY > 0;
+    this.scr=window.scrollY;
+    var a=1;
+    document.getElementById("d1").style.background="linear-gradient(to bottom, #252440"+(this.scr/10)+"%,#20525F"+(this.scr/20)+"%,#205273"+(this.scr/30)+"%,#205287"+(this.scr/40)+"%,#20529B"+(this.scr/50)+"%, #2052AF"+(this.scr/60)+"%, #2052C3"+(this.scr/70)+"%, #2052CD"+(this.scr/80)+"%, #2052E1"+(this.scr/90)+"%, #2052F5"+(this.scr/100)+"%)";
+    // document.getElementById("d2").style.opacity =590-(this.scr/(590)) 
+}},
+created () {
+  window.addEventListener('scroll', this.handleScroll);
+},
+destroyed () {
+  // window.removeEventListener('scroll', this.handleScroll);
+},
+});
 
 </script>
