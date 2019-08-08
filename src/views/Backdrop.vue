0b9916f05aa998f281c1cb0f8a3341fb80e9ce91
Anant Jain <anant.j2409@gmail.com>
2019-08-08 00:20:39 -0400

    Update Backdrop.vue

--- src/views/Backdrop.vue
+++ src/views/Backdrop.vue
@@ -1,15 +1,23 @@
 <template>
 <div class="app">
-	<div class="section1" id="d1">
+	<div class="section" id="d1">
       <h1>{{scr}}</h1>
-            </div>
-            </div>
+    </div>
+    <div class="section" id="d2"></div>
+    <div class="section" id="d3"></div>
+    <div class="section" id="d4"></div>
+    <div class="section" id="d5"></div>
+    <div class="section" id="d6"></div>
+    <div class="section" id="d7"></div>
+        <div class="section" id="d8"></div>
+            <div class="section" id="d9"></div>
+    </div>
 </template>
 <style scoped>
-.section1{
-background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
+.section{
   /* background-color: rgb(69, 111, 225); */
-  height:5000px;
+    background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
+  height:100vh;
 }
 h1 {
   position: fixed;
@@ -46,23 +54,24 @@ export default Vue.extend({
     this.scr=window.scrollY;
   var y = (window.scrollY || window.pageYOffset)
    if(y<440){
+     
      document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%)";}
   if(y>440){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,95,1) 0%, rgba(32,82,115,1)"+(y/20)+ "%)";}
+     document.getElementById("d2").style.background = "linear-gradient(180deg, rgba(32,82,95,1) 0%, rgba(32,82,115,1)"+(y/20)+ "%)";}
       if(y>880){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,115,1) 0%, rgba(32,82,135,1)"+(y/20)+ "%)";}
+     document.getElementById("d3").style.background = "linear-gradient(180deg, rgba(32,82,115,1) 0%, rgba(32,82,135,1)"+(y/20)+ "%)";}
       if(y>1320){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,135,1) 0%, rgba(32,82,155,1)"+(y/20)+ "%)";}
+     document.getElementById("d4").style.background = "linear-gradient(180deg, rgba(32,82,135,1) 0%, rgba(32,82,155,1)"+(y/20)+ "%)";}
     if(y>1760){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,155,1) 0%, rgba(32,82,175,1)"+(y/20)+ "%)";}
+     document.getElementById("d5").style.background = "linear-gradient(180deg, rgba(32,82,155,1) 0%, rgba(32,82,175,1)"+(y/20)+ "%)";}
   if(y>2200){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,175,1) 0%, rgba(32,82,195,1)"+(y/35)+ "%)";}
+     document.getElementById("d6").style.background = "linear-gradient(180deg, rgba(32,82,175,1) 0%, rgba(32,82,195,1)"+(y/35)+ "%)";}
   if(y>2640){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,195,1) 0%, rgba(32,82,205,1)"+(y/44)+ "%)";}
-       if(y>3080){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,205,1) 0%, rgba(32,82,225,1)"+(y/44)+ "%)";}
+     document.getElementById("d7").style.background = "linear-gradient(180deg, rgba(32,82,195,1) 0%, rgba(32,82,205,1)"+(y/44)+ "%)";}
+       if(y>3080){ 
+     document.getElementById("d8").style.background = "linear-gradient(180deg, rgba(32,82,205,1) 0%, rgba(32,82,225,1)"+(y/44)+ "%)";}
        if(y>3520){
-     document.getElementById("d1").style.background = "linear-gradient(180deg, rgba(32,82,225,1) 0%, rgba(32,82,245,1)"+(y/44)+ "%)";}
+     document.getElementById("d9").style.background = "linear-gradient(180deg, rgba(32,82,225,1) 0%, rgba(32,82,245,1)"+(y/44)+ "%)";}
 }},
 created () {
   window.addEventListener('scroll', this.handleScroll);
