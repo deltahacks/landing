56611622ad76bcb59f0eb02dddbe748a9be9eba5
amirafzali <amir@afzali.ca>
2019-08-09 15:53:17 -0400

    Applied new gradient approach

--- src/views/Backdrop.vue
+++ src/views/Backdrop.vue
@@ -1,24 +1,29 @@
 <template>
 <div class="app blen">
-  	<div class="section" id="d1"><h1>{{scr}}</h1></div>
-    <div class="section" id="d2"><a href="#d3">D2, Next -> d3</a></div>
-    <div class="section" id="d3"><a href="#d4">D3, Next -> d4</a></div>
-    <div class="section" id="d4"><a href="#d5">D4, Next -> d5</a></div>
-    <div class="section" id="d5"><a href="#d6">D5, Next -> d6</a></div>
-    <div class="section" id="d6"><a href="#d7">D6, Next -> d7</a></div>
-    <div class="section" id="d7"><a href="#d8">D7, Next -> d8</a></div>
-    <div class="section" id="d8"><a href="#d9">D8, Next -> d9</a></div>
-    <div class="section" id="d9"></div>
-    </div>
+    <div class="scroll"/>
+  	<div class="section" id="d1"><h1>{{scr}}</h1>
+</div>
+</div>
 </template>
 <style scoped>
 .blen {
    background-blend-mode: overlay;
 }
+.scroll {
+  top: 20px;
+  height: 900vh
+}
 .section {
   /* background-color: rgb(69, 111, 225); */
-  background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
+  background-image: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);
+  position: absolute;
+  background-size: cover;
+  background-position: center;
   height:100vh;
+  width:100vw;
+  top: 0;
+  left: 0;
+  position:fixed;
 }
 h1 {
   position: fixed;
@@ -39,14 +44,7 @@ export default Vue.extend({
   name: 'app',
   data() {
     return{
-      scrolled: false,
-      description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens!',
-      bgc: {
-        backgroundColor: '',
-      },
-      fin: 'background: linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%);',
       scr: 0,
-      col: '',
     };
   },
   methods: {
@@ -54,23 +52,24 @@ export default Vue.extend({
       this.scr = window.scrollY;
       const y = (window.scrollY || window.pageYOffset);
       if (y < 440) {
-        document.getElementById('d1')!.style.background = 'linear-gradient(180deg, rgba(37,36,64,1) 0%, rgba(32,82,95,1) 100%)'; }
-      if (y > 440) {
-        document.getElementById('d2')!.style.background = 'linear-gradient(180deg, rgba(32,82,95,1) 0%, rgba(32,82,115,1)' + (y / 12.8) + '%)'; }
-      if (y > 840) {
-        document.getElementById('d3')!.style.background = 'linear-gradient(180deg, rgba(32,82,115,1) 0%, rgba(32,82,135,1)' + (y / 23.44) + '%)'; }
-      if (y > 1504) {
-        document.getElementById('d4')!.style.background = 'linear-gradient(180deg, rgba(32,82,135,1) 0%, rgba(32,82,155,1)' + (y / 38.4) + '%)'; }
-      if (y > 2300) {
-        document.getElementById('d5')!.style.background = 'linear-gradient(180deg, rgba(32,82,155,1) 0%, rgba(32,82,175,1)' + (y / 54) + '%)'; }
-      if (y > 3100) {
-        document.getElementById('d6')!.style.background = 'linear-gradient(180deg, rgba(32,82,175,1) 0%, rgba(32,82,195,1)' + (y / 69) + '%)'; }
-      if (y > 3800) {
-        document.getElementById('d7')!.style.background = 'linear-gradient(180deg, rgba(32,82,195,1) 0%, rgba(32,82,205,1)' + (y / 84) + '%)'; }
-      if (y > 4600) {
-        document.getElementById('d8')!.style.background = 'linear-gradient(180deg, rgba(32,82,205,1) 0%, rgba(32,82,225,1)' + (y / 96) + '%)'; }
-      if (y > 5000) {
-        document.getElementById('d9')!.style.background = 'linear-gradient(180deg, rgba(32,82,225,1) 0%, rgba(32,82,245,1)' + (y / 114) + '%)'; }
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(37,36,64,1), rgba(32,65,95,1)' + Math.floor((-100 / 440) * y + 100) + '%, rgba(32,82,95,1) 100%)';
+      } else if (y < 840) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,65,95,1), rgba(32,82,95,1)' + Math.floor((-1 / 4) * y + 210) + '%, rgba(32,82,115,1) 100%)';
+      } else if (y < 1504) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,95,1), rgba(32,82,115,1)' + Math.floor((-25 / 166) * y + 226) + '%, rgba(32,82,135,1) 100%)';
+      } else if (y < 2300) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,115,1), rgba(32,82,135,1)' + Math.floor((-25 / 199) * y + 288) + '%, rgba(32,82,155,1) 100%)';
+      } else if (y < 3100) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,135,1), rgba(32,82,155,1)' + Math.floor((-1 / 8) * y + 388) + '%, rgba(32,82,175,1) 100%)';
+      } else if (y < 3800) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,155,1), rgba(32,82,175,1)' + Math.floor((-1 / 7) * y + 542) + '%, rgba(32,82,195,1) 100%)';
+      } else if (y < 4600) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,175,1), rgba(32,82,195,1)' + Math.floor((-1 / 8) * y + 575) + '%, rgba(32,82,205,1) 100%)';
+      } else if (y < 5000) {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,195,1), rgba(32,82,205,1)' + Math.floor((-1 / 4) * y + 1250) + '%, rgba(32,82,225,1) 100%)';
+      } else {
+        document.getElementById('d1')!.style.backgroundImage = 'linear-gradient(180deg, rgba(32,82,205,1), rgba(32,82,225,1), rgba(32,82,245,1) 100%)';
+      }
     },
   },
   created() {
