6920a5d12fa919223b15978ecaeefa57c2b1a88f
Anant Jain <anant.j2409@gmail.com>
2019-08-12 21:53:06 -0400

    Added Scroll_Reveal to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <Sponsors />
+    <Sponsors/>
+    <br>
+    <h1 v-scroll-reveal.reset >Test for Scroll Reveal</h1>
+    <h1 v-scroll-reveal.reset="{ delay: 250 }" >Another test for Scroll Reveal</h1>
   </div>
 </template>
 
@@ -10,6 +13,10 @@
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import Sponsors from '@/components/Sponsors.vue';
+import VueScrollReveal from 'vue-scroll-reveal';
+
+// Using ScrollReveal's default configuration
+Vue.use(VueScrollReveal);
 
 export default Vue.extend({
   name: 'home',
