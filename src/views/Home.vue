00b5274d2739cc3c68e9e99939ea315b718c9b41
Anant Jain <anant.j2409@gmail.com>
2019-08-14 21:39:26 -0400

    Trying to implement it

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <Sponsors />
+    <Sphere />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import Sponsors from '@/components/Sponsors.vue';
+import Sphere from '@/components/Sphere.vue'; 
 
 export default Vue.extend({
   name: 'home',
   components: {
     HelloWorld,
     Sponsors,
+    Sphere,
   },
 });
 </script>
