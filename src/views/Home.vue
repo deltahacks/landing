fc781f2ca03c234439d6df577f42ece0dab29ec7
Anant Jain <anant.j2409@gmail.com>
2019-08-17 10:28:26 -0400

    Resolving Merge Conflicts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <Sponsors />
-    <Sphere />
+   <HelloWorld
+      v-scroll-reveal.reset="{ delay: 100 }"
+      msg="Welcome to Your Vue.js + TypeScript App"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Landing from '@/components/Landing.vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import Sponsors from '@/components/Sponsors.vue';
 import Sphere from '@/components/Sphere.vue';
+const VueScrollReveal = require('vue-scroll-reveal');'
+
+// Using ScrollReveal's default configuration
+Vue.use(VueScrollReveal);
 
 export default Vue.extend({
   name: 'home',
   components: {
     HelloWorld,
-    Sponsors,
-    Sphere,
+    Landing,
   },
 });
 </script>
