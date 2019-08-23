ce330f2dc7ce2b61d1233cb21e162f237ba8e9a6
gucci-ninja <beautyspell5@gmail.com>
2019-08-23 08:34:03 -0400

    Made the animation less laggy

--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -26,22 +26,19 @@ export default Vue.extend({
       yVal: this.y,
       time: 0,
       phase: Math.random() * 2 * Math.PI,
-      period: 40 / (2 + this.depth),
+      period: 80 / (2 + this.depth),
       amplitude: this.depth / 4,
-      interval: 0,
     };
   },
   mounted() {
-    this.interval = setInterval(this.tick, 30);
+    requestAnimationFrame(this.tick);
   },
   methods: {
     tick() {
       this.time += 1;
       this.yVal += this.amplitude * Math.sin(this.time * 0.5 / this.period + this.phase);
+      requestAnimationFrame(this.tick);
     },
-  },
-  beforeDestroy() {
-    clearInterval(this.interval);
   },
 });
 </script>
