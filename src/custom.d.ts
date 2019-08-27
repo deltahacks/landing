ab9ae9c8128eeef8bbc929077df5efc64502e8ab
Anant Jain <anant.j2409@gmail.com>
2019-08-26 22:49:46 -0400

    Linting

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,8 +1,8 @@
-declare module "*.svg" {
+declare module '*.svg' {
     const value: any;
     export default value;
 }
-declare module "*.png" {
+declare module '*.png' {
     const value: any;
     export default value;
 }
