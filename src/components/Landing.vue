3ae82f241908351689014d3accc6b0df00077e9d
Anant Jain <anant.j2409@gmail.com>
2019-10-28 16:44:18 -0400

    Styling Changes

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="flex-container">
-    <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-white.svg" alt="Major League Hacking 2020 Hackathon Season" style="width:100%"></a>
+    <a id="mlh-trust-badge" class="mlh-badge" href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-white.svg" alt="Major League Hacking 2020 Hackathon Season" style="width:100%"></a>
     <div id="particles-js"></div>
     <img class="main-planet" :src="Planet" />
     <div id="left">
@@ -443,7 +443,21 @@ export default Vue.extend({
   object-fit: contain;
 }
 
+.mlh-badge{
+  display:block;
+  max-width:100px;
+  min-width:60px;
+  position:fixed;
+  right:50px;
+  top:0;
+  width:10%;
+  z-index:10000;
+}
+
 @media only screen and (max-width: 900px) {
+  .mlh-badge{
+    display: none !important;
+  }
   .flex-container {
     display: flex;
     flex-direction: column;
