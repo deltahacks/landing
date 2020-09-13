45004804e1ea1dd90ae53d39340da3c308cf5877
Anant Jain <anant.j2409@gmail.com>
2020-09-13 14:19:52 -0400

    Added mailing List option

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -29,11 +29,20 @@
           </div>
         </div>
         </div>
-             <div class="grey-dh7-blob2">
+        <div class="grey-dh7-blob2">
         <div class="grey-dh7-blob2-inner">
           <p style="font-size: 13px; font-weight:600;">HACKER APPLICATIONS DROPPING IN T-MINUS</p>
           <h1 style="font-size:55px; margin:0px;">00 : 00 : 00</h1>
         </div>
+        </div>
+        <div class="grey-dh7-blob3">
+          <div class="grey-dh7-blob3-inner">
+            <p style="font-size: 20px; font-weight:600; margin-bottom:10px;">DH7 MAILING LIST</p>
+          <input class="grey-dh7-blob3-input" placeholder="Your Name"/>
+          <input class="grey-dh7-blob3-input" placeholder="Your Email"/>
+          <br>
+            <button class="grey-dh7-blob3-button" onclick="alert('Wow you are on the waiting list lmaoo!')">Submit</button>
+          </div>
         </div>
       <!-- <div class="landing-left-container"> -->
         <!-- <a
@@ -512,6 +521,49 @@ clip-path: polygon(0% 0%, 100% 0%, 100% 70%, 90% 100%, 0% 100%);
 }
 .grey-dh7-blob2-inner{
   margin: 0 20px;
+}
+.grey-dh7-blob3{
+  margin-top: 20px!important;
+clip-path: polygon(0% 0%, 100% 0%, 100% 80%, 90% 100%, 0% 100%);
+  background-color: #464646;
+  width: 380px;
+  height: 200px;
+ margin-right:200px!important ;
+ padding:0px;
+  /* margin-left: 300px; */
+  /* align-items: center;
+  text-align: center; */
+  display: inline-flex; 
+}
+.grey-dh7-blob3-inner{
+  margin: 0 0 0 0;
+  padding:0px;
+}
+.grey-dh7-blob3-input{
+  margin: 5px 0 5px 0;
+  width:300px;
+  height:35px;
+  background-color:#333333;
+  border:0px;
+  text-align:center;
+  color: white !important;
+  cursor: pointer;
+   padding:0px;
+}
+.grey-dh7-blob3-input::placeholder{
+  color:#DCDCDC !important;
+}
+.grey-dh7-blob3-button{
+  clip-path: polygon(0% 0%, 100% 0%, 100% 70%, 90% 100%, 0% 100%);
+  margin-right:200px;
+  background-color:#333333;
+  border:0px;
+  height:35px;
+  width: 100px;
+  color:white;
+  margin-top:5px;
+  cursor: pointer;
+   padding:0px;
 }
 .main-planet {
   width: 40%;
