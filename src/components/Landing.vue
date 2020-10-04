25fffedfb85f8b17f8fbe85e27b046bbcfb0a7ae
Anant Jain <anant.j2409@gmail.com>
2020-10-04 10:48:42 -0400

    Reverting Landing Back to Original

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -17,41 +17,18 @@
     <img class="main-planet" :src="Planet" />
     <div id="left">
       <h1 class="landing-title">
-      Delta<span style="font-weight: 300">Hacks</span>
-        7
-      <p style=" margin:0px 0px 0px -285px; font-size:20px;">at McMaster University</p>
+        Delta<span style="font-weight: 300">Hacks</span>
+        VI
       </h1>
-      <img class="vi-back" :src="VI" /> 
-      <div class="grey-dh7-blob1">
-        <div class="grey-dh7-blob1-inner">
-          <div class="grey-dh7-blob1-navlink">
-        <a>Schedule</a>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;<a>Challenges</a>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;<a>Workshops</a>
-          </div>
-        </div>
-        </div>
-        <div class="grey-dh7-blob2">
-        <div class="grey-dh7-blob2-inner">
-          <p style="font-size: 13px; font-weight:600;">HACKER APPLICATIONS DROPPING IN T-MINUS</p>
-          <h1 style="font-size:55px; margin:0px;">00 : 00 : 00</h1>
-        </div>
-        </div>
-        <div class="grey-dh7-blob3">
-          <div class="grey-dh7-blob3-inner">
-            <p style="font-size: 20px; font-weight:600; margin-bottom:10px;">DH7 MAILING LIST</p>
-          <input class="grey-dh7-blob3-input" placeholder="Your Name"/>
-          <input class="grey-dh7-blob3-input" placeholder="Your Email"/>
-          <br>
-            <button class="grey-dh7-blob3-button" onclick="alert('Wow you are on the waiting list lmaoo!')">Submit</button>
-          </div>
-        </div>
-      <!-- <div class="landing-left-container"> -->
-        <!-- <a
+      <img class="vi-back" :src="VI" />
+      <div class="landing-left-container">
+        <a
           v-if="!enteringName && !enteringEmail && !gotit"
           class="landing-button fade-in"
           @click="enteringName = true"
         >
           DH7 Mailing list
-        </a> -->
+        </a>
         <!-- <a
           v-if="!enteringName && !enteringEmail && !gotit"
           class="landing-button fade-in"
@@ -67,7 +44,7 @@
         >
           Sponsor
         </a>-->
-        <!-- <div v-if="enteringName" class="email-group fade-in">
+        <div v-if="enteringName" class="email-group fade-in">
           <div class="email-button" @click="(enteringName = false), (enteringEmail = false)">
             <i
               class="fa fa-arrow-circle-o-left fa-3x"
@@ -116,7 +93,7 @@
           v-if="!enteringName && !enteringEmail && !gotit"
           class="landing-date"
         >McMaster University</span>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -407,8 +384,6 @@ export default Vue.extend({
   color: white;
   padding-top: 15%;
   font-weight: 600;
-  margin-bottom: 10px !important;
-  margin-right:55px;
 }
 .landing-left-mlh {
   margin: 2vh 0 0vh;
@@ -490,82 +465,6 @@ export default Vue.extend({
   position: relative;
 }
 
-.grey-dh7-blob1{
-clip-path: polygon(0% 0%, 100% 0%, 100% 30%, 90% 100%, 0% 100%);
-  background-color: #464646;
-  width: 380px;
-  height: 70px;
-  margin-right:200px!important ;
-  /* margin-left: 300px; */
-  /* align-items: center;
-  text-align: center; */
-  display: inline-flex; 
-}
-.grey-dh7-blob1-inner{
-  margin: auto 25px;
-}
-.grey-dh7-blob1-navlink{
-  color:white;
-  cursor: pointer!important;
-}
-.grey-dh7-blob2{
-  margin-top: 20px!important;
-clip-path: polygon(0% 0%, 100% 0%, 100% 70%, 90% 100%, 0% 100%);
-  background-color: #464646;
-  width: 380px;
-  height: 115px;
-  margin-right:200px!important ;
-  /* margin-left: 300px; */
-  /* align-items: center;
-  text-align: center; */
-  display: inline-flex; 
-}
-.grey-dh7-blob2-inner{
-  margin: 0 20px;
-}
-.grey-dh7-blob3{
-  margin-top: 20px!important;
-clip-path: polygon(0% 0%, 100% 0%, 100% 80%, 90% 100%, 0% 100%);
-  background-color: #464646;
-  width: 380px;
-  height: 200px;
- margin-right:200px!important ;
- padding:0px;
-  /* margin-left: 300px; */
-  /* align-items: center;
-  text-align: center; */
-  display: inline-flex; 
-}
-.grey-dh7-blob3-inner{
-  margin: 0 0 0 0;
-  padding:0px;
-}
-.grey-dh7-blob3-input{
-  margin: 5px 0 5px 0;
-  width:300px;
-  height:35px;
-  background-color:#333333;
-  border:0px;
-  text-align:center;
-  color: white !important;
-  cursor: pointer;
-   padding:0px;
-}
-.grey-dh7-blob3-input::placeholder{
-  color:#DCDCDC !important;
-}
-.grey-dh7-blob3-button{
-  clip-path: polygon(0% 0%, 100% 0%, 100% 70%, 90% 100%, 0% 100%);
-  margin-right:200px;
-  background-color:#333333;
-  border:0px;
-  height:35px;
-  width: 100px;
-  color:white;
-  margin-top:5px;
-  cursor: pointer;
-   padding:0px;
-}
 .main-planet {
   width: 40%;
   height: 45%;
