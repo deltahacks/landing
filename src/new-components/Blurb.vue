fb6680b76a23197293c3165be0f44a5b60d07197
Collin Kan <kanc@mcmaster.ca>
2020-11-15 16:10:14 -0500

    Change font size for mobile

--- src/new-components/Blurb.vue
+++ src/new-components/Blurb.vue
@@ -7,6 +7,9 @@
                 <input class="button" type="submit" :value="buttonText">
             </form>
         </div>
+        <form :class='align' v-if="hasButton" :action="buttonLink">
+            <input :class="['mbutton', align]" type="submit" :value="buttonText">
+        </form>
     </div>
 </template>
 
@@ -16,7 +19,7 @@ export default Vue.extend({
     props: {
         align: {
             type: String,
-            default: 'left',
+            default: 'right',
         },
         header: {
             type: String,
@@ -70,14 +73,13 @@ export default Vue.extend({
 .heading {
     color: white;
     padding: 2% 3%;
-    font-size: 4vmin;
+    font-size: 4.5vmin;
     font-weight: 600;
     margin: 0;
 }
 
 .description {
-    padding: 2% 3%;
-    margin: 2.5vh inherit;
+    padding: 1% 3%;
     z-index: 40;
     background: rgb(85, 85, 85);
     padding-bottom: 2vh;
@@ -104,42 +106,73 @@ export default Vue.extend({
     transition: 0.2s;
 }
 
+.mbutton {
+    color:white;
+    outline: none;
+    border: none;
+    padding: 2vmin 3vmin 2vmin 3vmin;
+    font-size: 4.5vmin;
+    font-weight: 550;
+    background-color: rgb(30, 30, 30);
+    cursor: pointer;  
+    -webkit-appearance: none;
+    display: none;
+}
+.mbutton:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: 0.2s;
+}
+.mbutton:active {
+    background-color: rgba(0, 0, 0, 0.25);
+    transition: 0.2s;
+}
+
 p {
     color:white;
-    font-size: 2.2vmin;
+    font-size: 2.4vmin;
 }
 
 @media screen and (max-width: 720px){
     .heading {
-        font-size: 35px;
+        font-size: 7.25vmin;
+        font-weight: 650;
+        margin-bottom: 2vmin;
+    }
+    p {
+        color:white;
+        font-size: 3.75vmin;
+    }
+    .description {
+        padding: 1vmin 1vmin 1vmin 1vmin;
+        z-index: 40;
+        background: rgb(85, 85, 85);
     }
     .button {
-        font-size: 17px;
+        display: none;
+    }
+
+    .mbutton {
+        display: initial;
     }
 
     #corner.right{
-        clip-path: polygon(0 0, 100% 0, 100% 100%, 15vmin 100%, 0 calc(100% - 15vmin));
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 12.5vmin 100%, 0 calc(100% - 12.5vmin));
     }
 
     #corner.left{
-        clip-path: polygon(0 0, 100% 0, 100% calc(100% - 15vmin), calc(100% - 15vmin) 100%, 0 100%);
+        clip-path: polygon(0 0, 100% 0, 100% calc(100% - 12.5vmin), calc(100% - 12.5vmin) 100%, 0 100%);
     }
 
     #corner.center{
-        clip-path: polygon(0 0, 100% 0, 100% calc(100% - 15vmin), calc(100% - 15vmin) 100%, 15vmin 100%, 0 calc(100% - 15vmin));
+        clip-path: polygon(0 0, 100% 0, 100% calc(100% - 12.5vmin), calc(100% - 12.5vmin) 100%, 12.5vmin 100%, 0 calc(100% - 12.5vmin));
     }
 
     .container {
         width: 100%;
-        padding: 0;
         text-align: center;
     }
     .container > h2 {
         padding: 0 5%;
-    }
-    .description {
-        margin-right: 4vw;
-        margin-left: 4vw;
     }
     .description > p {
         padding: 0 5%;
