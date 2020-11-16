eaf2bcb9a2eb2e51ce1891f15c424315eefbb167
Amir Afzali <amir@afzali.ca>
2020-11-16 01:59:35 -0500

    Remove old routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Faq from './new-components/Faq.vue';
-import Backdrop from './views/Backdrop.vue';
 
 Vue.use(Router);
 
