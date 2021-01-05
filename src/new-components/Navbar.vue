8864baa79bbfe05ba93ebc17caca97e59e5490ab
Shubh Sehgal <shubhsehgal007@gmail.com>
2021-01-05 17:27:18 -0500

    fix contacts link

--- src/new-components/Navbar.vue
+++ src/new-components/Navbar.vue
@@ -13,8 +13,8 @@
     <a class="link" v-scroll-to="'#sponsor'" @scroll="handleScroll">
       Sponsors
     </a>
-    <a class="link" v-scroll-to="'#footer'" @scroll="handleScroll">
-      Contact
+    <a class="link" href="mailto:hello@deltahacks.com" target=“_blank”> 
+      Contact 
     </a>
     <p class="bar">|</p>
     <a href="https://www.my.deltahacks.com" id="login" class="link">Login</a>
