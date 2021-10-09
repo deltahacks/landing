f2bb7b4275ad23b5de55ccbb7dfa97531c7145d0
Anant Jain <anant.j2409@gmail.com>
2021-10-09 17:20:24 -0400

    Updated badge

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -4,11 +4,11 @@
     <a
       id="mlh-trust-badge"
       class="mlh-badge"
-      href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white"
+      href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
       target="_blank"
     >
       <img
-        src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-white.svg"
+        src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg"
         alt="Major League Hacking 2020 Hackathon Season"
         style="width:100%"
       />
--- src/new-components/Landing.vue
+++ src/new-components/Landing.vue
@@ -6,17 +6,17 @@
     <img class="bruh2" src="../assets/cyber/title_nostars2.svg" />
 
     <div class="flex-container">
-      <a
-        id="mlh-trust-badge"
-        class="mlh-badge"
-        href="https://mlh.io/seasons/2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white"
-        target="_blank"
-      >
-        <img
-          src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg"
-          alt="Major League Hacking 2020 Hackathon Season"
-          style="width: 100%"
-        />
+    <a
+      id="mlh-trust-badge"
+      class="mlh-badge"
+      href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
+      target="_blank"
+    >
+      <img
+        src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg"
+        alt="Major League Hacking 2020 Hackathon Season"
+        style="width:100%"
+      />
       </a>
       <div id="particles-js"></div>
       <div id="left">
